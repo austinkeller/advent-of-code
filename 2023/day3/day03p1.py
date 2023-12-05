1c44d575e5e86ec814ffaa6e71ee5719de79b2ba
Austin Keller <austin474@gmail.com>
2023-12-04 21:50:11 -0800

    Day 3

---
+++ 2023/day3/day03p1.py
@@ -0,0 +1,54 @@
+import re
+
+total_sum = 0
+
+matrix = []
+
+lines = []
+
+len_line = None
+
+# Open input file
+with open(0) as f:
+    # Read file
+    for i, line in enumerate(f.readlines()):
+        line = line.strip()
+        if len_line is None:
+            len_line = len(line)
+        assert len(line) == len_line
+        lines.append(line)
+        matrix.append([])
+        for c in line:
+            matrix[i].append(c)
+
+[print(f"{x.group()}, {x.start(0)}, {x.end(0)}") for x in re.finditer(r'(\d+)', "467..114..")]
+
+
+def is_near_symbol(start: int, end: int, col: int):
+    print(f"start: {start}, end: {end}, col: {col}")
+    for i in range(max(start - 1, 0), min(end + 1, len(matrix[col]))):
+        for j in range(max(0, col - 1), min(len(matrix), col + 2)):
+            if not re.match(r'\d|\.', matrix[j][i]):
+                # Found a symbol
+                assert not matrix[j][i].isdigit()
+                assert matrix[j][i] != '.'
+                print(f"Symbol: {matrix[j][i]}")
+                return True
+    return False
+              
+
+# Read file
+for i, line in enumerate(lines):
+    # if i > 3:
+    #     break
+    print(i)
+    print(line)
+    numbers = re.finditer(r'(\d+)', line)
+    for num in numbers:
+        print(f"num: {int(num.group())}")
+        if is_near_symbol(num.start(0), num.end(0), i):
+            print("FOUND")
+            total_sum += int(num.group())
+
+
+print(f"Total sum: {total_sum}")
---
+++ 2023/day3/day03p2.py
@@ -0,0 +1,44 @@
+import re
+
+total_sum = 0
+
+lines: list[str] = []
+
+# Open input file
+with open(0) as f:
+    # Read file
+    for i, line in enumerate(f.readlines()):
+        line = line.strip()
+        lines.append(line)
+
+
+# Read file
+for line_num, line in enumerate(lines):
+    symbols = re.finditer(r'(\*)', line)
+    for symbol in symbols:
+        numbers = []
+        # Find digits
+        for i in range(line_num - 1, line_num + 2):
+            if i < 0 or i >= len(lines):
+                continue
+            for j in range(symbol.start(0) - 1, symbol.end(0) + 1):
+                if j < 0 or j >= len(line):
+                    continue
+                if lines[i][j].isdigit():
+                    if any([number["start"] <= j and number["end"] > j and i == number["row"] for number in numbers]):
+                        # already saw number
+                        continue
+                    # Found number, extract it
+                    low = j
+                    while low > 0 and lines[i][low - 1].isdigit():
+                        low -= 1
+                    high = j
+                    while high < len(line) and lines[i][high].isdigit():
+                        high += 1
+                    numbers.append({"value": int(lines[i][low:high]), "start": low, "end": high, "row": i})
+        if len(numbers) == 2:
+            # Found a gear
+            total_sum += numbers[1]["value"] * numbers[0]["value"]
+
+
+print(f"Total sum: {total_sum}")
