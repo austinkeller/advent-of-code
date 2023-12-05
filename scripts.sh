7f5b922df45cadfd6500f02cab63b613d732dba0
Austin Keller <austin474@gmail.com>
2023-12-05 01:16:51 -0800

    Reorganize

--- scripts.sh
+++ scripts.sh
@@ -1,15 +1,6 @@
-# Add these to your ~/.bash_aliases
-
-set AOC="~/git/github.com/austinkeller/advent-of-code" # remember to change this to whatever your AOC directory is
-set AOC_COOKIE="..." # get this from the cookies tab in network tools on the AOC website
-
 alias aos="cd $AOC; python3 solution.py < in.txt"
 alias aot="cd $AOC; echo -ne '\\e[0;34m'; python3 solution.py < test.txt; echo -ne '\\e[0m'"
 alias aoc="aot; echo; aos"
-
-alias jaos="cd $AOC; bun solution.js in.txt"
-alias jaot="cd $AOC; bun solution.js test.txt"
-alias jaoc="jaot; echo; jaos"
 
 function aoc-load () {
     if [ $1 ]
